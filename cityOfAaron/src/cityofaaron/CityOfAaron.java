8565d29075a8913eb2734248269eb9a2655e349e
mariacarreno <car17083@byui.edu>
2018-10-05 22:47:36 -0500

    05/10/2018 - created project CityOfAaron

---
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * The main()class file for the cityOfAaron project
+ * CIT 260
+ * Spring 2018
+ * Team members: Nate Petersen, Rhett McDermott, María Carreño.
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
