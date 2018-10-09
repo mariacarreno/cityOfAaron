60cd25f2c37f0c7252e86e758bcbcaf67bcd428f
Nate Petersen <natepetersen@gmail.com>
2018-10-08 21:58:59 -0600

    10/08/2018 - Added comments that correspond to the instructions given on slide 62.

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -20,16 +20,35 @@ public class CityOfAaron {
      */
     public static void main(String[] args) {
         
+        /* 
+         * Create an instance of your Player class. 
+         * Using its setName() method, store a person’s 
+         * name in the instance of your Player class 
+         */
         Player player1 = new Player();
         player1.setName("Test Player");
         
+        /* 
+         * Create an instance of your Game class. 
+         * Using its setPlayer() method, save a 
+         * reference to your player object in the 
+         * game object 
+         */
         Game game1 = new Game();
         game1.setPlayer(player1);
         
+        /* 
+         * Get a reference to the Player object from the game 
+         * object, and use its getName() method to display it 
+         */
         System.out.println(game1.getPlayer().getName());
         
+        /* 
+         * Get one of the objects in your TeamMember enum and call its getName() 
+         * and getDescription() methods to retrieve the name and description for 
+         * that team member and display them on the console 
+         */
         System.out.println(TeamMember.TeamMember1.getName());
-        
         System.out.println(TeamMember.TeamMember1.getTitle());
         
     }
