88cea7a56cef6829114dd2fc58838c0fd7810b6f
Nate Petersen <natepetersen@gmail.com>
2018-10-08 21:21:40 -0600

    10/08/2018 - Initial commit of model/Player.java

---
+++ cityOfAaron/src/model/Player.java
@@ -0,0 +1,28 @@
+/*
+ * The Player class file for the cityOfAaron project
+ * CIT 260
+ * Spring 2018
+ * Team members: Nate Petersen, Rhett McDermott, María Carreño.
+ */
+
+package model;
+
+/**
+ *
+ * @author npetersen
+ */
+public class Player {
+    
+    private String name;
+    
+    public Player() {}
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String _name) {
+        this.name = _name;
+    }
+    
+}
