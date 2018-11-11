33a148a246520745df9eb80ade4405b1a93b5824
Nate Petersen <natepetersen@gmail.com>
2018-11-10 23:05:22 -0700

    11/10/2018 - Fixed issues per week 8's powerpoint slides.

--- cityOfAaron/src/View/GameMenuView.java
+++ cityOfAaron/src/View/GameMenuView.java
@@ -17,40 +17,11 @@ import control.*;
 public class GameMenuView {
 
     private final String theGameMenu;
-    String userInput;
+    private final int max = 5;
+    int userInput = 0;
     Scanner keyboard = new Scanner(System.in);
 
-    // The displayMenuView method
-    // Purpose: displays the menu, gets the user's input, and does the 
-    // selected action
-    // Parameters: none
-    // Returns: none
-    // =========================================================    
-    public void displayMenuView() {
-        
-        String menuOption;
-        boolean endView = false;
-        
-        do {
-            // Display the menu
-            System.out.println(theGameMenu);
-
-            // Prompt the user and get the user’s input
-            menuOption = getMenuOption();
-            
-            if (menuOption.equals("Q") || menuOption.equals("")) {
-                return;
-            } else {
-                endView = true;
-            }
-
-            // Perform the desired action
-            doAction(menuOption);
-
-        } while (endView != true);
-
-    }
-
+    
     // The GameMenuView constructor
     // Purpose: Initialize the game menu data
     // Parameters: none
@@ -61,49 +32,60 @@ public class GameMenuView {
                 + "**********************************\n"
                 + "* CITY OF AARON: GAME MENU  *\n"
                 + "**********************************\n"
-                + " V - View map\n"
-                + " I - View/Print a list\n"
-                + " A - View list of actors\n"
-                + " S - View ship status\n"
-                + " L - View contents of location\n"
-                + " M - Move person to new location\n"
-                + " E - Estimate the resource needed\n"
-                + " B - Design barrels\n"
-                + " C - Construct tools\n"
-                + " R - Harvest resource\n"
-                + " D - Deliver resource\n"
-                + " W - Work on ship\n"
-                + " J - Launch ship\n"
-                + " SG - Save game\n"
-                + " H - Help\n"
-                + " Q - Quit\n";
+                + " 1 - View Map\n"
+                + " 2 - View/Print a List\n"
+                + " 3 - Move to a New Location\n"
+                + " 4 - Manage the Crops\n"
+                + " 5 - Return to the Main Menu\n";
+    }
+    
+
+    // The displayMenuView method
+    // Purpose: displays the menu, gets the user's input, and does the 
+    // selected action
+    // Parameters: none
+    // Returns: none
+    // =========================================================
+    public void displayMenuView() {
+        
+        int menuOption;
+        
+        do {
+            // Display the menu
+            System.out.println(theGameMenu);
+
+            // Prompt the user and get the user’s input
+            menuOption = getMenuOption();
+
+            // Perform the desired action
+            doAction(menuOption);
+        
+        } while (menuOption != max);
+    
     }
     
 
     // The getInputs method
     // Purpose: gets the user's input
     // Parameters: none
-    // Returns: String - the option selected
+    // Returns: int - the option selected
     // ===================================       
-    public String getMenuOption() {
-        
-        String[] inputs = new String[] {"V", "I", "A", "S", "L", "M", "E", "B", "C", "R", "D", "W", "J", "SG", "H", "Q"};
-        
-        boolean valid = false;
+    public int getMenuOption() {
         
         do {
-            
             // get user input from the keyboard
-            userInput = keyboard.next().trim().toUpperCase();
+            userInput = keyboard.nextInt();
             
-            if (userInput.length() > 0 && Arrays.stream(inputs).anyMatch(userInput::equals)) {
-                valid = true;
-            } else {
-                System.out.println("Error: you must select a valid menu option.");
+            // if it is not a valid value, output an error message
+            if (userInput < 1 || userInput > max) {
+                System.out.println("Error: you must select 1, 2, 3, 4, or 5");
             }
-            
-        } while (valid != true);
-        
+
+            // loop back to the top of the loop if input was not valid
+
+            // end loop
+        } while (userInput < 1 || userInput > max);
+
         return userInput;
         
     }
@@ -111,162 +93,56 @@ public class GameMenuView {
 
     // The doAction method
     // Purpose: performs the selected action
-    // Parameters: none
+    // Parameters: int menu option
     // Returns: none
     // ===================================       
-    public void doAction(String option) {
+    public void doAction(int option) {
         switch (option) {
-            case "V":
+            case 1: //view map
                 viewMap();
                 break;
-            case "I": //view lists menu
+            case 2: //view lists menu
                 viewList();
                 break;
-            case "A":
-                viewActors();
+            case 3: //view move to new location
+                moveToNewLocation();
                 break;
-            case "S":
-                viewShipStatus();
+            case 4: // manage crops
+                manageCrops();
                 break;
-            case "L":
-                viewLocationContents();
-                break;
-            case "M":
-                movePerson();
-                break;
-            case "E":
-                estimateNeededResources();
-                break;
-            case "B":
-                designBarrles();
-                break;
-            case "C":
-                constructTools();
-                break;
-            case "R":
-                harvestResource();
-                break;
-            case "D":
-                deliverResource();
-                break;
-            case "W":
-                workOnShip();
-                break;
-            case "J":
-                launchShip();
-                break;
-            case "SG":
-                saveGame();
-                break;
-            case "H":
-                help();
-                break;
-            case "Q":
-                break;
-
+            case 5: // main menu
+                return;
         }
 
     }
 
-    // The startNewGame method
-    // Purpose: creates game object and starts the game
-    // Parameters: none
-    // Returns: none
+    
     // ===================================     
     public void viewMap() {
         // Display Stub.
-        System.out.println("\nDisplay Map.");
+        System.out.println("\nView Map.");
     }
+    
 
     // ===================================     
     public void viewList() {
         // Display Stub.
-         ListMenuView lmv = new ListMenuView();
-         lmv.displayMenuView();
+        ListMenuView lmv = new ListMenuView();
+        lmv.displayMenuView();
     }
+    
   
     // ===================================     
-    public void viewActors() {
+    public void moveToNewLocation() {
         // Display Stub.
-        System.out.println("\nDisplay Actors.");
+        System.out.println("\nMove to New Location.");
     }
+    
 
     // ===================================     
-    public void viewShipStatus() {
+    public void manageCrops() {
         // Display Stub.
-        System.out.println("\nDisplay Ship Status.");
-    }
-    
-    // ===================================     
-    public void viewLocationContents() {
-        // Display Stub.
-        System.out.println("\nDisplay Location Contents.");
-    }
-    
-    // ===================================     
-    public void movePerson() {
-        // Display Stub.
-        System.out.println("\nMove Person to New Location.");
-    }
-    
-    // ===================================     
-    public void estimateNeededResources() {
-        // Display Stub.
-        System.out.println("\nEstimate Needed Resources.");
-    }
-    
-    // ===================================     
-    public void designBarrles() {
-        // Display Stub.
-        System.out.println("\nDesign Barrels.");
-    }
-    
-    // ===================================     
-    public void constructTools() {
-        // Display Stub.
-        System.out.println("\nConstruct Tools.");
-    }
-    
-    // ===================================     
-    public void harvestResource() {
-        // Display Stub.
-        System.out.println("\nHarvest Resource.");
-    }
-    
-    // ===================================     
-    public void deliverResource() {
-        // Display Stub.
-        System.out.println("\nDeliver Resource.");
-    }
-    
-    // ===================================     
-    public void workOnShip() {
-        // Display Stub.
-        System.out.println("\nWork on Ship.");
-    }
-    
-    // ===================================     
-    public void packShip() {
-        // Display Stub.
-        System.out.println("\nPack Ship.");
-    }
-    
-    // ===================================     
-    public void launchShip() {
-        // Display Stub.
-        System.out.println("\nLaunch Ship.");
-    }
-    
-    // ===================================     
-    public void saveGame() {
-        // Display Stub.
-        System.out.println("\nSave Game.");
-    }
-    
-    // ===================================     
-    public void help() {
-        // Display Stub.
-        System.out.println("\nHelp.");
+        System.out.println("\nManage Crops.");
     }
 
 }
